737e28dfceec29c5f7b57956e588e3b8699b5a04
andriisydor <andrii.sydor.knm.2020@lpnu.ua>
2023-11-26 21:37:39 +0200

    Try to load and show data

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
-print("hello!")
+from pyspark.sql import SparkSession
+from pyspark import SparkConf
+
+from settings import DATA_DIRECTORY_PATH, TRIP_FARE_PATH
+from app.read_write import CSVManager
+
+
+def main():
+    spark_session = (SparkSession.builder
+                     .master('local')
+                     .appName('big_data_2023')
+                     .config(conf=SparkConf())
+                     .getOrCreate())
+
+    csv_manager = CSVManager(spark_session, DATA_DIRECTORY_PATH)
+
+    trip_fare_df = csv_manager.read(TRIP_FARE_PATH)
+    trip_fare_df.show()
+
+
+if __name__ == '__main__':
+    main()
---
+++ settings.py
@@ -0,0 +1,7 @@
+
+DATA_DIRECTORY_PATH = 'data'
+
+# in data directory:
+TRIP_FARE_PATH = "/trip_fare_1.csv"
+
+
