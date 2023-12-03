67d315d947d6d4eed7ab48460814e0de97591589
andriisydor <andrii.sydor.knm.2020@lpnu.ua>
2023-12-03 13:34:12 +0200

    Remove useless prints

--- main.py
+++ main.py
@@ -24,12 +24,9 @@ def main():
     number_of_rows_to_show = 20
 
     print(f'Rows count: {trip_fare_df.count()}')
-    print(f'First {number_of_rows_to_show} rows:')
     trip_fare_df.show(number_of_rows_to_show)
     print(f'Dataframe schema: {trip_fare_df.schema}')
-    print('Columns description: ')
     trip_fare_df.describe().show()
-    print('Columns summary: ')
     trip_fare_df.summary().show()
     pickup_datetime_min, pickup_datetime_max = timestamp_column_min_and_max(trip_fare_df, columns.pickup_datetime)
     print(f'pickup_datetime min: {pickup_datetime_min}, max: {pickup_datetime_max}')
