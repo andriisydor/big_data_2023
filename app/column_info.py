d11e25967a7632675569c37d58677ce494f84c41
andriisydor <andrii.sydor.knm.2020@lpnu.ua>
2023-12-03 13:28:14 +0200

    Remove functions that print

--- app/column_info.py
+++ app/column_info.py
@@ -1,30 +1,12 @@
 """ Contains tools to analise dataset columns. """
-from app import columns
 
 
-def gather_and_print_dataframe_info(dataframe):
-    """Prints some analytical information about dataframe.
-
-    Args:
-        dataframe (pyspark.sql.DataFrame): Dataframe for analysis.
-    """
-
-    number_of_rows_to_show = 20
-    print(f'Rows count: {dataframe.count()}')
-    print(f'First {number_of_rows_to_show} rows:')
-    dataframe.show(number_of_rows_to_show)
-    print(f'Dataframe schema: {dataframe.schema}')
-    print('Columns description: ')
-    dataframe.describe().show()
-    print('Columns summary: ')
-    dataframe.summary().show()
+def timestamp_column_min_and_max(dataframe, column_name):
+    min_timestamp = dataframe.select(column_name).agg({column_name: "min"}).head()[0]
+    max_timestamp = dataframe.select(column_name).agg({column_name: "max"}).head()[0]
+    return min_timestamp, max_timestamp
 
 
-def print_timestamp_column_min_and_max(dataframe, column_name):
-    dataframe.select(column_name).agg({column_name: "min"}).show()
-    dataframe.select(column_name).agg({column_name: "max"}).show()
-
-
-def print_trip_fare_info(trip_fare_dataframe):
-    gather_and_print_dataframe_info(trip_fare_dataframe)
-    print_timestamp_column_min_and_max(trip_fare_dataframe, columns.pickup_datetime)
+def number_of_unique_values_of_column(dataframe, column_name):
+    unique_values = dataframe.select(column_name).distinct()
+    return unique_values.count()
--- main.py
+++ main.py
@@ -5,7 +5,8 @@ from settings import DATA_DIRECTORY_PATH, TRIP_FARE_PATH, TRIP_DATA_PATH
 from schemas import trip_data_schema, trip_fare_schema
 
 from app.CSVManager import CSVManager
-from app.column_info import print_trip_fare_info
+from app import columns
+from app.column_info import timestamp_column_min_and_max
 from app.transformations import strip_names_of_columns
 
 
@@ -20,11 +21,22 @@ def main():
 
     trip_fare_df = csv_manager.read(TRIP_FARE_PATH, trip_fare_schema)
     trip_fare_df = strip_names_of_columns(trip_fare_df)
-    print_trip_fare_info(trip_fare_df)
+    number_of_rows_to_show = 20
+
+    print(f'Rows count: {trip_fare_df.count()}')
+    print(f'First {number_of_rows_to_show} rows:')
+    trip_fare_df.show(number_of_rows_to_show)
+    print(f'Dataframe schema: {trip_fare_df.schema}')
+    print('Columns description: ')
+    trip_fare_df.describe().show()
+    print('Columns summary: ')
+    trip_fare_df.summary().show()
+    pickup_datetime_min, pickup_datetime_max = timestamp_column_min_and_max(trip_fare_df, columns.pickup_datetime)
+    print(f'pickup_datetime min: {pickup_datetime_min}, max: {pickup_datetime_max}')
 
     trip_data_df = csv_manager.read(TRIP_DATA_PATH, trip_data_schema)
     print(trip_data_df.schema)
-    trip_data_df.show(20)
+    trip_data_df.show(number_of_rows_to_show)
 
 
 if __name__ == '__main__':
