c73a2d253e2cc5b4a234aa77ffc5ae0eff7edde1
yuliamarkiv <yuliia.markiv.knm.2020@lpnu.ua>
2023-12-10 19:50:55 +0200

    fixed PR comments

--- app/QueryManager.py
+++ app/QueryManager.py
@@ -1,5 +1,5 @@
 from pyspark.sql import Window
-from  pyspark.sql.functions import col, date_format, desc, max, sum, format_number, avg, lit, asc
+import pyspark.sql.functions as F
 from app import columns
 
 
@@ -21,64 +21,80 @@ class QueryManager:
             3. Count (count of trips)
         """
         trip_df = self.trip_data_df.withColumn('dayofweek',
-                                       date_format(self.trip_data_df[date_column], 'EEEE'))
-        trips_by_week = (trip_df.filter(col(columns.vendor_id) != 'None').groupBy(columns.vendor_id, 'dayofweek').
-                         count().orderBy(desc(columns.vendor_id), desc('count')).withColumn('max_trip_count',
-                                                                                            max('count').over(
+                                               F.date_format(self.trip_data_df[date_column], 'EEEE'))
+        trips_by_week = (trip_df.filter(F.col(columns.vendor_id) != 'None').groupBy(columns.vendor_id, 'dayofweek').
+                         count().orderBy(F.desc(columns.vendor_id), F.desc('count')).withColumn('max_trip_count',
+                                                                                            F.max('count').over(
                                                                                                 Window.partitionBy(
                                                                                                     'vendor_id')))
-                         .filter(col('count') == col('max_trip_count')).drop('max_trip_count'))
+                         .filter(F.col('count') == F.col('max_trip_count')).drop('max_trip_count'))
         return trips_by_week
 
     def total_revenue(self):
-        """ Calculates the total revenue of each vendor """
-        dataframe = (self.trip_fare_df.filter(col(columns.vendor_id) != 'None').groupBy(columns.vendor_id)
-                     .agg(format_number(sum(columns.total_amount), 2).alias('total revenue')))
+        """ Calculates the total revenue of each vendor
+             Returns:
+            DataFrame: A DataFrame containing the total revenue for each vendor.
+        """
+        dataframe = (self.trip_fare_df.filter(F.col(columns.vendor_id) != 'None').groupBy(columns.vendor_id)
+                     .agg(F.format_number(F.sum(columns.total_amount), 2).alias('total revenue')))
         return dataframe
 
     def avg_trip_distance(self):
-        """ Calculates the average trip distance for different amount of passengers """
-        dataframe = (self.trip_data_df.filter(col(columns.passenger_count).
+        """
+            Calculates the average trip distance for different numbers of passengers.
+
+            Returns:
+                DataFrame: A DataFrame containing the average trip distance for each combination of vendor and passenger count.
+        """
+        dataframe = (self.trip_data_df.filter(F.col(columns.passenger_count).
                                               isNotNull()).groupBy(columns.vendor_id, columns.passenger_count).
-                     agg(avg(columns.trip_distance)).orderBy(desc(columns.passenger_count)))
+                     agg(F.avg(columns.trip_distance)).orderBy(F.desc(columns.passenger_count)))
         return dataframe
 
     def simultaneous_trips(self):
-        """ Calculates how many trips happened simultaneously """
-        pickup_dataframe = (self.trip_data_df.filter(col(columns.pickup_datetime).isNotNull()).
-                            select(col(columns.pickup_datetime).alias('event_time'),
-                                   lit(1).alias('event_count')))
-        dropoff_dateframe = (self.trip_data_df.filter(col(columns.dropoff_datetime).isNotNull()).
-                             select(col(columns.dropoff_datetime).alias('event_time'),
-                                    lit(-1).alias('event_count')))
+        """
+        Calculates the maximum number of simultaneous trips that happened on the same day.
+
+        Returns:
+            DataFrame: A DataFrame containing the maximum number of simultaneous trips for the top 5 days.
+        """
+        pickup_dataframe = (self.trip_data_df.filter(F.col(columns.pickup_datetime).isNotNull()).
+                            select(F.col(columns.pickup_datetime).alias('event_time'),
+                                   F.lit(1).alias('event_count')))
+        dropoff_dateframe = (self.trip_data_df.filter(F.col(columns.dropoff_datetime).isNotNull()).
+                             select(F.col(columns.dropoff_datetime).alias('event_time'),
+                                    F.lit(-1).alias('event_count')))
         event_dateframe = pickup_dataframe.union(dropoff_dateframe)
-        dataframe = event_dateframe.withColumn('sum', sum('event_count').over(Window.partitionBy('event_time')
-                                                                              .orderBy(asc('event_time'))))
-        dataframe = dataframe.groupBy(date_format('event_time', 'yyyy-MM-dd').alias('day')
-                                      ).agg(max('sum').alias('simultaneous_trips')).orderBy(
-            desc(col('simultaneous_trips'))).limit(5)
+        dataframe = event_dateframe.withColumn('sum', F.sum('event_count').over(Window.partitionBy('event_time')
+                                                                              .orderBy(F.asc('event_time'))))
+        dataframe = dataframe.groupBy(F.date_format('event_time', 'yyyy-MM-dd').alias('day')
+                                      ).agg(F.max('sum').alias('simultaneous_trips')).orderBy(
+            F.desc(F.col('simultaneous_trips'))).limit(5)
         return dataframe
 
     def most_expensive_trips(self):
-        """Calculates the most expensive trips by vendor """
-        dataframe = self.trip_fare_df.groupBy(columns.vendor_id).agg(max(columns.total_amount).
+        """
+        Calculates the most expensive trips for each vendor.
+
+        Returns:
+            DataFrame: A DataFrame containing the most expensive trips for each vendor.
+        """
+        dataframe = self.trip_fare_df.groupBy(columns.vendor_id).agg(F.max(columns.total_amount).
                                                                      alias(columns.total_amount))
         return dataframe
 
     def avg_amount_rate_code(self):
-        """ Calculates count of trips the tip above average tip amount for trips with different rate code. """
-        dataframe = self.trip_fare_df.join(
-                    self.trip_data_df,
-                    [self.trip_data_df[columns.medallion] == self.trip_fare_df[columns.medallion],
-                     self.trip_data_df[columns.hack_license] == self.trip_fare_df[columns.hack_license],
-                     self.trip_data_df[columns.pickup_datetime] == self.trip_fare_df[columns.pickup_datetime]],
-                    'inner'
-                )
-        average_tip_amounts = dataframe.groupBy(columns.rate_code).agg(avg(columns.tip_amount).alias('avg_tip_amount'))
+        """
+        Calculates the count of trips with a tip above the average tip amount for trips with different rate codes.
+
+        Returns:
+            DataFrame: A DataFrame containing the count of such trips for each rate code.
+        """
+        dataframe = self.trip_fare_df.join(self.trip_data_df, ['medallion', 'hack_license', 'vendor_id',
+                                                               'pickup_datetime'], 'inner')
+        average_tip_amounts = dataframe.groupBy(columns.rate_code).agg(F.avg(columns.tip_amount).alias('avg_tip_amount'))
         joined_data = dataframe.join(average_tip_amounts, on=columns.rate_code, how='inner')
-        dataframe = joined_data.withColumn('tip_above_avg', col('tip_amount') > col('avg_tip_amount'))
+        dataframe = joined_data.withColumn('tip_above_avg', F.col('tip_amount') > F.col('avg_tip_amount'))
         dataframe = (dataframe.groupBy(columns.rate_code).count().withColumnRenamed('count', 'trip_count').
-                     orderBy(desc('trip_count')))
+                     orderBy(F.desc('trip_count')))
         return dataframe
-
-
