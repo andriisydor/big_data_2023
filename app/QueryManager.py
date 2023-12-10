9c475fc46244d8961942609d5acd8d92165756ec
andriisydor <andrii.sydor.knm.2020@lpnu.ua>
2023-12-10 20:07:00 +0200

    Add more business questions

--- app/QueryManager.py
+++ app/QueryManager.py
@@ -1,5 +1,7 @@
 from pyspark.sql import Window
 from pyspark.sql.functions import col, date_format, desc, max
+import pyspark.sql.functions as f
+from app import columns
 
 
 class QueryManager:
@@ -29,3 +31,61 @@ class QueryManager:
                                                                                               "vendor_id")))
                          .filter(col("count") == col("max_trip_count")).drop("max_trip_count"))
         return trips_by_week
+
+    def trips_with_tip_mount_greater_than_fare_amount(self):
+        """ Data of trips with tips amount greater than the fare amount.
+
+        Returns:
+            dataframe with columns:
+            medallion, hack_license, vendor_id, pickup_datetime, payment_type, fare_amount, tip_amount.
+        """
+
+        result_columns_names = [columns.medallion, columns.hack_license, columns.vendor_id, columns.pickup_datetime,
+                                columns.payment_type, columns.fare_amount, columns.tip_amount]
+        trips_with_tip_mount_greater_than_fare_amount = (
+            self.trip_fare_df.filter(col(columns.fare_amount) < col(columns.tip_amount)).select(*result_columns_names)
+        )
+        return trips_with_tip_mount_greater_than_fare_amount
+
+    def total_earnings_of_each_vendor_for_first_seven_days_of_january(self):
+        """ Sum of earning of each vendor for trips that started on each of the first seven days of January 2013.
+
+        Returns:
+            dataframe with columns:
+            vendor_id, date(in format yyyy-MM-dd), total_earnings.
+        """
+        column_date = 'date'
+        column_total_earnings = 'total_earnings'
+        start_date_string = '2012-12-31 23:59:59.59'
+        end_date_string = '2013-01-07 23:59:59.59'
+
+        total_earnings_of_each_vendor_for_first_seven_days_of_january = (
+            self.trip_fare_df
+                .withColumn(column_date, date_format(self.trip_fare_df[columns.pickup_datetime], 'yyyy-MM-dd'))
+                .filter(col(column_date).between(start_date_string, end_date_string))
+                .orderBy(columns.vendor_id, column_date)
+                .groupBy(columns.vendor_id, column_date)
+                .agg(f.sum(columns.total_amount).alias(column_total_earnings))
+        )
+        return total_earnings_of_each_vendor_for_first_seven_days_of_january
+
+    def driver_of_each_day(self):
+        """
+        """
+        column_date = 'date'
+        column_tips_sum = 'tips_sum'
+        column_max_tips_sum = 'max_tips_sum'
+        join_column_names = [columns.vendor_id, columns.medallion, columns.hack_license, columns.pickup_datetime]
+        joined_df = self.trip_fare_df.join(self.trip_data_df, join_column_names, 'left')
+        drivers = (
+            joined_df
+                .withColumn('date', f.date_format(joined_df[columns.dropoff_datetime], 'yyyy-MM-dd'))
+                .groupBy(columns.vendor_id, columns.hack_license, column_date)
+                .agg(f.sum(columns.tip_amount).alias(column_tips_sum))
+                .orderBy(column_date, f.desc(column_tips_sum))
+                .withColumn(column_max_tips_sum, f.max(f.col(column_tips_sum))
+                            .over(Window.partitionBy(column_date)).alias(column_max_tips_sum))
+                .filter(f.col(column_max_tips_sum) == f.col(column_tips_sum))
+                .select(column_date, columns.hack_license, columns.vendor_id, column_tips_sum)
+        )
+        return drivers
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from pyspark.sql import SparkSession
 from pyspark import SparkConf
 
-from settings import DATA_DIRECTORY_PATH, TRIP_FARE_PATH, TRIP_DATA_PATH
+from settings import DATA_DIRECTORY_PATH, TRIP_FARE_PATH, TRIP_DATA_PATH, OUTPUT_DIRECTORY
 from schemas import trip_data_schema, trip_fare_schema
 
 from app.CSVManager import CSVManager
@@ -12,14 +12,21 @@ from app.transformations import strip_names_of_columns, clean_dataframe
 from app.QueryManager import QueryManager
 
 
-def business_questions(spark, trip_fare_df, trip_data_df):
+def business_questions(query_manager, csv_manager, output_directory):
     """all business methods will be invoked here """
-    query_manager = QueryManager(spark, trip_fare_df, trip_data_df)
     # Which day of the week has the highest number of trips?
     trips_fare_by_week = query_manager.trips_count(trip_fare_df, "pickup_datetime")
     trips_data_by_week = query_manager.trips_count(trip_data_df, "pickup_datetime")
     trips_fare_by_week.show(20)
     trips_data_by_week.show(20)
+
+    # trips_with_tip_mount_greater_than_fare_amount = query_manager.trips_with_tip_mount_greater_than_fare_amount()
+    # csv_manager.write(trips_with_tip_mount_greater_than_fare_amount, f'{output_directory}/andrii/1')
+    # total_earnings_of_each_vendor_for_january = \
+    #     query_manager.total_earnings_of_each_vendor_for_first_seven_days_of_january()
+    # csv_manager.write(total_earnings_of_each_vendor_for_january, f'{output_directory}/andrii/2')
+    driver_of_each_day = query_manager.driver_of_each_day()
+    csv_manager.write(driver_of_each_day, f'{output_directory}/andrii/3')
 
 
 def main():
@@ -47,7 +54,8 @@ def main():
     trip_fare_df = clean_dataframe(trip_fare_df)
     trip_data_df = clean_dataframe(trip_data_df)
 
-    business_questions(spark_session, trip_fare_df, trip_data_df)
+    query_manager = QueryManager(spark_session, trip_fare_df, trip_data_df)
+    business_questions(query_manager, csv_manager, OUTPUT_DIRECTORY)
 
 
 if __name__ == '__main__':
